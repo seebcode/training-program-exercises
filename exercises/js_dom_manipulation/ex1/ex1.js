b79cf607e9f336a63e6ffa3f712c7bc89c865c18
Sebastien Singarayar <z5444568@ad.unsw.edu.au>
2024-09-19 20:10:11 +1000

    finished exercise 1

--- exercises/js_dom_manipulation/ex1/ex1.js
+++ exercises/js_dom_manipulation/ex1/ex1.js
@@ -1,4 +1,13 @@
 console.log("Yay! The javascript is running")
 
-// Cat picture source 
+// Cat picture source
 // https://cdn-prd.content.metamorphosis.com/wp-content/uploads/sites/6/2022/12/shutterstock_781327003-1-768x512.jpg
+document.querySelector("#name").innerText = "Seb";
+document.querySelector("#bigger").style.color = "green";
+document.querySelector("#bigger").style.fontSize = "100px";
+document.querySelector('img').src = "https://cdn.betterttv.net/emote/658f28f95e7e78960777148d/3x.webp";
+
+const squares = document.getElementsByClassName("square");
+for (const square of squares) {
+  square.style.display = "unset";
+}
