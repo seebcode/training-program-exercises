ceed336fe3cfddddf3c2e1ac18071b316234cd44
Sebastien Singarayar <z5444568@ad.unsw.edu.au>
2024-09-19 20:43:41 +1000

    finished exercise 2

--- exercises/js_dom_manipulation/ex2/ex2.js
+++ exercises/js_dom_manipulation/ex2/ex2.js
@@ -1,3 +1,21 @@
 console.log("Exercise 2")
 
 const main = document.querySelector("#display-box")
+
+document.getElementById("button1").onmouseleave = function() {changeHtml(1)};
+document.getElementById("button2").onmouseleave = function() {changeHtml(2)};
+document.getElementById("button3").onmouseleave = function() {changeHtml(3)};
+
+let changeHtml = (buttonNumber) => {
+  switch(buttonNumber) {
+    case 1:
+      document.querySelector('#display-box').innerHTML = document.querySelector('#text1').innerHTML;
+      break;
+    case 2:
+      document.querySelector('#display-box').innerHTML = document.querySelector('#text2').innerHTML;
+      break;
+    case 3:
+      document.querySelector('#display-box').innerHTML = document.querySelector('#text3').innerHTML;
+      break;
+  }
+}
